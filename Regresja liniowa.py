185509530dc60ffdcd1116baac64e7942479b51f
Longina <nina@wp.pl>
2024-01-13 09:07:46 +0100

    New directory

---
+++ Regresja liniowa.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+pd.read_csv('weight-height.csv')
---
+++ tmp.py
@@ -0,0 +1,8 @@
+#matplotlib.pyplot #wykres
+#seaborn # ładny wykres
+#pandas # do czytania danych
+#tensorflow, keras #sztuczna inteligencja
+#selenium #testy webowe
+#numpy, scipy #matematyka, obliczenia
+#tkinter, pyqt6 #grafika
+#pygame #gry
